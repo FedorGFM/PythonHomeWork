7d8bf354f4fc4eaf8172886f350e760e393a8a80
Fedor <4ips228@gmail.com>
2023-04-02 19:12:47 +0700

    class work

---
+++ Task39_3_star.py
@@ -0,0 +1,15 @@
+def group_letter(input_list):
+
+
+    word_dict = {}
+    for word in input_list:
+        if (frozenset(word), len(word)) not in word_dict:
+            word_dict[(frozenset(word), len(word))] = [word]
+        else:
+            word_dict[(frozenset(word), len(word))].append(word)
+    res_list = []
+    for value in word_dict.values():
+        res_list.append(value)
+    return res_list
+
+print(group_letter(["eat", "tea", "tan", "ate", "nat", "bat", 'batt', 'b', 'cadfsdf']))
